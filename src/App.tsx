cfca5cf50d507696621dcf9a2430b14530367aa3
Tobias Mahnke <tobmah@users.noreply.github.com>
2021-09-06 14:47:36 +0200

    access ble battery percentage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const [device, setDevice] = useState(null);
+
   return (
     <div className="App">
     <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
+      <button disabled={device !== null} onClick={async () => {
+        try {
+
+          function handleBatteryLevelChanged(event: React.ChangeEvent<HTMLInputElement>) {
+            // @ts-expect-error
+            const batteryLevel = event.target.value.getUint8(0);
+            console.log('Battery percentage is ' + batteryLevel);
+          }
+
+          // @ts-expect-error
+          const blDevice = await navigator.bluetooth.requestDevice({
+            acceptAllDevices: true,
+            optionalServices: ['battery_service'] // Required to access service later.
+          });
+
+          const server = await blDevice.gatt.connect();
+          const batteryService = await server.getPrimaryService('battery_service');
+          const batteryCharacteristic = await batteryService.getCharacteristic('battery_level');
+          const batteryValue = await batteryCharacteristic.readValue();
+
+          batteryCharacteristic.addEventListener('characteristicvaluechanged', handleBatteryLevelChanged);
+
+          console.log('device', blDevice);
+          console.log('server', server);
+          console.log('batteryService', batteryService);
+          console.log('batteryCharacteristic', batteryCharacteristic);
+          console.log('batteryValue', batteryValue);
+
+          setDevice(blDevice);
+        } catch(err) {
+          console.log('error on requestDevice', err);
+        }
+      }}>Request Bluetooth device</button>
+      <button disabled={device === null} onClick={() => {
+        if(!device) {
+          return
+        }
+        // @ts-expect-error
+        device.gatt.disconnect();
+        setDevice(null);
+      }}>Disconnect from Bluetooth device</button>
     </header>
     </div>
   )
