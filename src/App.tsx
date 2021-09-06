f38043d0c5b5a789105b5ffd20297e7555be66eb
Tobias Mahnke <tobmah@users.noreply.github.com>
2021-09-06 13:57:24 +0200

    basic app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </header>
+    </div>
+  )
+}
+
+
+export default App;
